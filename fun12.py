f4174884006365b311ec0a0ea9a9e1837a4f986f
Bal-Krishna-Yadav <168990174+Bal-Krishna-Yadav@users.noreply.github.com>
2025-04-22 19:12:48 +0530

    Add files via upload

---
+++ fun12.py
@@ -0,0 +1,12 @@
+def calculate_bonus(salary):
+    print("original salary:", salary)
+    salary = salary + 5000  # bonus added
+    print("original salary with bonus):", salary)
+
+# Main code
+employee_salary = 30000
+print("Before employee salary:", employee_salary)
+
+calculate_bonus(employee_salary)
+
+print("After employee salary:", employee_salary)
