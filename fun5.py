54a6e7ef0b7e61fe37e7660cce91fea571a4fdbe
Bal-Krishna-Yadav <168990174+Bal-Krishna-Yadav@users.noreply.github.com>
2025-04-22 19:07:42 +0530

    Add files via upload

---
+++ fun5.py
@@ -0,0 +1,37 @@
+def BCA_4th_Sem(*Syllabus):
+   for Study in Syllabus:
+      print(Study)
+Subject=["Java Programming","Python Programming",
+    "Operating System","Software Engineering","Java "
+        "Programming Lab","Python Programming Lab","English"
+            "Communication Lab","Disaster Management","Indian "
+                "Constitution"]  
+
+BCA_4th_Sem(*Subject)
+
+print("--------Bal--Krishna--Kumar--------------")
+
+def BCA_5th_Sem(*Syllabus):
+   for Study in Syllabus:
+      print(Study)
+Subject=["Statistical Methods","Project-II","Training viva-voce",
+    "Internet of Things","Big Data Analytics",
+        "Big Data Analytics Lab","Data Science",
+            "Data Science Lab","Data Warehousing & Data Mining",
+                "Entrepreneurship Development & Management"
+                    "Cognitive Skills Training-I"]  
+
+BCA_5th_Sem(*Subject)
+
+print("--------Bal--Krishna--Kumar--------------")
+
+def BCA_6th_Sem(*Syllabus):
+   for Study in Syllabus:
+      print(Study)
+Subject=["R Programming","R Programming Lab",
+    "Machine Learning","Project-III",
+        "Data Visualization","Data Visualization Lab",
+            "Cultural/ Sports/Health & Yoga",
+                "Cognitive Skills Training - II"] 
+ 
+BCA_5th_Sem(*Subject)
