872edd6d7c2dde8098c0a1e617efb0a96b95a8d8
Bal-Krishna-Yadav <168990174+Bal-Krishna-Yadav@users.noreply.github.com>
2025-04-22 19:08:22 +0530

    Add files via upload

---
+++ fun6.py
@@ -0,0 +1,23 @@
+# ➕ Addition with 4 positional arguments
+def add_num(a, b, c, d):
+    sum = a + b + c + d
+    print("Sum of four numbers:", sum)
+add_num(10, 20, 30, 40)
+
+# ➖ Subtraction with 2 positional arguments
+def sub_num(x, y):
+    sub = x - y
+    print("Sub of two numbers:", sub)
+sub_num(50, 20)
+
+# ✖️ Multiplication with 2 positional arguments
+def mul_num(e, f):
+    mul = e * f
+    print("Mul of two numbers:", mul)
+mul_num(5, 5)
+
+# ➗ Division with 2 positional arguments
+def div_num(g, h):
+    div = g / h
+    print("Div of two numbers:", div)
+div_num(20, 10)
