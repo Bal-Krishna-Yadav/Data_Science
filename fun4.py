df55bc2fcd1a9bec51198ed1bc365076aae2fbdd
Bal-Krishna-Yadav <168990174+Bal-Krishna-Yadav@users.noreply.github.com>
2025-04-22 19:07:06 +0530

    Add files via upload

---
+++ fun4.py
@@ -0,0 +1,38 @@
+def BCA_1th_Sem(*Syllabus):
+   for Study in Syllabus:
+      print(Study)
+Subject=["Programmin in C","C lab","English",
+    "Computer Fundamental & Ms Office",
+        "Digital Electronics","Ms Office Lab",
+            "Information Technology",
+                "Universal Human Values"]   
+
+BCA_1th_Sem(*Subject)
+
+print("--------Bal--Krishna--Kumar--------------")
+
+def BCA_2nd_Sem(*Syllabus):
+   for Study in Syllabus:
+      print(Study)
+Subject=["ObjectOriented Programming using C++",
+    "Data Structure","System Analysis & Design",
+        "Hindi/Essence of Indian Traditional Knowledge",
+             "Environment Studies","C++ Lab",
+                "Data Structure Lab.","Seminar-I",
+                    "Cultural/Sports/Health & Yoga"]
+   
+BCA_2nd_Sem(*Subject)
+
+print("--------Bal--Krishna--Kumar--------------")
+
+def BCA_3rd_Sem(*Syllabus):
+   for Study in Syllabus:
+      print(Study)
+Subject=["Database Management System","Web Designing "
+    "Fundamentals","Computer System Architecture",
+        "Mathematical Foundation","Data Communication & "
+            "Networking","DBMS Lab",
+                "Web Designing Lab","Project-I",
+                    "NSS/ NCC"] 
+ 
+BCA_3rd_Sem(*Subject)
