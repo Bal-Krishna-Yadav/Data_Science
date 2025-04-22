4d3781c9518a4b288dbc88c557f8ce22c6af05ad
Bal-Krishna-Yadav <168990174+Bal-Krishna-Yadav@users.noreply.github.com>
2025-04-22 19:06:39 +0530

    Add files via upload

---
+++ fun3.py
@@ -0,0 +1,21 @@
+def adress(Name,Roll="1",Hostel="12"):
+    print("Name of Student:",Name)
+    print("Roll of Student",Roll)
+    print("Roll of Student",Hostel)
+    
+def Department(Name,ID="MCM & MBM"):
+    print("Name of Management:",Name)
+    print("ID of MCM & MBM",ID)
+   
+def MCM(Name,ID="MCM"):
+    print("Name of Department:",Name)
+    print("ID of Department",ID)
+
+def MBM(Name,ID="MCM"):
+    print("Name of Department:",Name)
+    print("ID of Department",ID)
+
+adress("Bal Krishna Kumar")
+Department("MVM & MBM")
+MCM("BCA")
+MBM("BCA")
